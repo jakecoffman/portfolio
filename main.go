5a577d4d1273a0335fae846c9ed8e4793f57ff0a
Jake Coffman <jcoffman@appistry.com>
2013-11-27 15:25:44 -0600

    Starting to refactor so I can use angular. Projects are currently broken.

--- main.go
+++ main.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
-	"github.com/jakecoffman/portfolio/handlers"
 	"github.com/jakecoffman/portfolio/utils"
-	"html/template"
 	"log"
 	"math/rand"
 	"net"
@@ -21,8 +18,6 @@ const port = ":80"
 var password = os.Getenv("emailpassword")
 
 var logFile *os.File
-
-var router *mux.Router
 
 func Emailer(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
@@ -40,9 +35,11 @@ func Emailer(w http.ResponseWriter, r *http.Request) {
 			payload := []byte(fmt.Sprintf("Subject: Portfolio contact\r\n\r\nEmail: %s\r\nSubject: %s\r\nMessage:\r\n%s", email, subject, message))
 			err := smtp.SendMail("smtp.gmail.com:587", auth, "no-reply@coffshire.com", to, payload)
 			if err != nil {
-				session.AddFlash("Failed to send email")
+				w.Write([]byte(err.Error()))
+				return
 			} else {
-				session.AddFlash("Email sent.")
+				http.Redirect(w, r, "/email-sent.html", http.StatusFound)
+				return
 			}
 		}
 		session.Save(r, w)
@@ -51,39 +48,11 @@ func Emailer(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/#contact", http.StatusFound)
 }
 
-func Projects(w http.ResponseWriter, r *http.Request) {
-	defer func() {
-		if r := recover(); r != nil {
-			http.Error(w, "project not found", http.StatusNotFound)
-		}
-	}()
-
-	vars := mux.Vars(r)
-	project := vars["project"]
-
-	t := template.Must(template.New("base.html").Funcs(utils.FuncMap).ParseFiles(
-		"web/templates/base.html",
-		"web/templates/projects.html",
-		fmt.Sprintf("web/templates/projects/%s.html", project),
-		"web/templates/side.html",
-	))
-
-	context := make(map[string]string)
-	context["project"] = project
-
-	if err := t.Execute(w, context); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	// Before
-	router.ServeHTTP(w, r)
-	// After
-	fmt.Fprintf(logFile, "%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
-}
-
 func main() {
+	if password == "" {
+		println("*** 'emailpassword' env var not set, emails won't be sent ***")
+	}
+
 	wd, _ := os.Getwd()
 	println("Working directory", wd)
 
@@ -97,20 +66,15 @@ func main() {
 
 	rand.Seed(time.Now().UTC().UnixNano())
 
+	http.HandleFunc("/contact", Emailer)
+	http.Handle("/", http.FileServer(http.Dir("web/")))
+
 	server := &http.Server{Addr: port, Handler: nil}
 	l, e := net.Listen("tcp", port)
 	if e != nil {
 		panic(e)
 	}
 	defer l.Close()
-
-	router = mux.NewRouter()
-	router.HandleFunc("/", handlers.IndexHandler)
-	router.HandleFunc("/contact", Emailer)
-	router.HandleFunc("/projects/{project}", Projects)
-	router.PathPrefix("/static/").Handler(http.FileServer(http.Dir("web/")))
-
-	http.HandleFunc("/", handler)
 
 	go func() {
 		fmt.Println("Running on " + port)
