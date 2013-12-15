b0b910992d0a6eb61f431e3840062b0b1cd45b33
jake <jake@coffshire.com>
2013-12-14 19:55:10 -0600

    redesign I won't be rolling out. committing in case I have second thoughts.

--- web/static/angular/app.js
+++ web/static/angular/app.js
@@ -3,10 +3,15 @@ var app = angular.module('portfolio', ['ngRoute']);
 app.config(['$routeProvider', function($routeProvider){
 	$routeProvider.when("/", {
 		templateUrl: "/templates/index.html"
-	});
-	$routeProvider.when("/projects/:project", {
+	}).when("/projects", {
+		templateUrl: "/templates/projects.html",
+		controller: 'ProjectCtl'
+	}).when("/projects/:project", {
 		templateUrl: function(params) { return "/templates/projects/" + params.project + ".html" },
 		controller: 'ProjectCtl'
+	}).when("/contact", {
+		templateUrl: "/templates/contact.html",
+		controller: 'MainCtl'
 	}).otherwise({
 		redirectTo: "/"
 	})
@@ -29,19 +34,25 @@ app.controller('MainCtl', ['$scope', '$http', function($scope, $http){
 	$scope.message = "";
 	$scope.subject = "";
 	$scope.contacted = "";
+	$scope.sending = false;
 
-	$scope.contacto = function(email, subject, message){
+	$scope.contacto = function(){
+		$scope.sending = true;
+		var email = $scope.email;
+		var subject = $scope.subject;
+		var message = $scope.message;
 		if(email === "" && message === "" && subject === "") {
 			$scope.contacted = "Try entering something first";
+			$scope.sending = false;
 			return;
 		}
 		$scope.contacted = "";
 
 		var data = {
-				email: email,
-				subject: subject,
-				message: message
-			};
+			email: email,
+			subject: subject,
+			message: message
+		};
 
 		console.log(data);
 
@@ -51,8 +62,10 @@ app.controller('MainCtl', ['$scope', '$http', function($scope, $http){
 			data: data
 		}).success(function(){
 			$scope.contacted = "Message sent";
+			$scope.sending = false;
 		}).error(function(data){
 			$scope.contacted = data;
+			$scope.sending = false;
 		})
 	};
 
@@ -75,7 +88,34 @@ app.controller('ProjectCtl', ['$scope', '$routeParams', function($scope, $routeP
 	}];
 
 	$scope.project = $routeParams.project;
-	$scope.all = ['alarm', 'desk', 'flask_tutorial', 'gameoflife', 'gorunner', 'jobs', 'taskpy'];
+	$scope.all = [{
+		page: 'alarm',
+		color: '#632E9A'
+	},{
+		page: 'desk',
+		color: '#02A200'
+	}, {
+		page: 'flask_tutorial',
+		color: '#082AB0'
+	}, {
+		page: 'gameoflife',
+		color: '#B9008A'
+	}, {
+		page: 'gorunner',
+		color: '#00B8BA'
+	}, {
+		page: 'jobs',
+		color: '#FF5B27'
+	}];
+
+	$scope.getColor = function(page) {
+		for(var i=0; i<$scope.all.length; i++) {
+			if($scope.all[i].page == page){
+				return $scope.all[i].color;
+			}
+		}
+		return "white";
+	}
 }]);
 
 var logos = [{
